0d4509173be1d893aa402759c3df8f9ee1b3fcd7
Berkan Karayel <berkankarayel.147@gmail.com>
2024-12-05 00:31:29 +0300

    if-else

---
+++ c#EğitimKampı/C# Kontrol Yapıları/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace C__Kontrol_Yapıları
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			// Easy  Problem : Check Leap Year 
+
+			// qUESTİON: 
+
+			/*
+				write a program that checks whether a given  year 
+			is a leap year or not .
+			 
+			 */
+
+			Console.WriteLine("enter a year : ");
+			int year = Convert.ToInt32(Console.ReadLine());
+			if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+			{
+				Console.WriteLine($"{year} is a leap year ");
+			}
+			else
+			{
+				Console.WriteLine($"{year} is not a leap year ");
+			}
+			Console.ReadLine();
+
+
+
+
+			// Hard Problem (Zor): Triangle Type Detector
+			//Write a program to determine the type of a triangle based on the lengths of its sides provided by the user.
+
+			//If all three sides are equal, it is an Equilateral Triangle.
+			//If two sides are equal, it is an Isosceles Triangle.
+			//If all three sides are different, it is a Scalene Triangle.
+			//If the given sides do not form a triangle, output "Not a valid triangle".
+
+
+			Console.WriteLine("Enter side 1: ");
+			int side1 = Convert.ToInt32(Console.ReadLine());
+
+			Console.WriteLine("Enter side 2: ");
+			int side2 = Convert.ToInt32(Console.ReadLine());
+
+			Console.WriteLine("Enter side 3: ");
+			int side3 = Convert.ToInt32(Console.ReadLine());
+
+			// check if the sides form a valid triangle 
+
+			if (side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1)
+			{
+				if (side1 == side2 && side2 == side3)
+				{
+					Console.WriteLine("the triangle is equilateral");
+				}
+				else if (side1 == side2 || side1 == side3 || side2 == side3)
+				{
+					Console.WriteLine("the triangle is Isosceles");
+				}
+				else
+				{
+					Console.WriteLine("the triangle is scalene");
+				}
+
+
+			}
+			else
+			{
+				Console.WriteLine("Not a valid triangle");
+			}
+			Console.ReadLine();
+		}
+	}
+}
---
+++ c#EğitimKampı/C# Kontrol Yapıları/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("C# Kontrol Yapıları")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("C# Kontrol Yapıları")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("2bcd651a-a56f-4f87-9dea-90163da1f0e9")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
