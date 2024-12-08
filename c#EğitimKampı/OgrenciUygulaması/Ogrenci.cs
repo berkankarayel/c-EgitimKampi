1532cf397fc926852ace65bd91c8cd1faf93f21d
Berkan Karayel <berkankarayel.147@gmail.com>
2024-12-09 00:17:10 +0300

    Uygulama

---
+++ c#EğitimKampı/OgrenciUygulaması/Ogrenci.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OgrenciUygulaması
+{
+	internal class Ogrenci
+	{
+		private int ogrNo;
+		private string isim;
+		private string soyisim;
+		private double vize1;
+		private double vize2;
+		private double final;
+		private string OkulIsmI;
+
+
+        public Ogrenci(string isim,string soyisim, double vize1,double vize2,double final,string OkulIsmi)
+        {
+			Random random = new Random();
+			int ilkUcHane = 201; // İlk üç hane sabit
+			int sonUcHane = random.Next(100, 1000); // Rastgele 3 haneli sayı
+			this.ogrNo = int.Parse($"{ilkUcHane}{sonUcHane}"); // 201xxx formatında öğrenci no
+			this.isim = isim;
+			this.soyisim = soyisim;
+			this.vize1 = vize1;
+			this.vize2 = vize2;
+			this.final = final;
+			this.OkulIsmI = OkulIsmi;
+        }
+
+		public void OgrenciBilgileriGoster()
+		{
+            Console.WriteLine("\n... Öğrenci Bilgileri .....");
+            Console.WriteLine($"Öğrenci No: {ogrNo}");
+            Console.WriteLine($"İsim : {isim}");
+            Console.WriteLine($"Soyisim : {soyisim}");
+            Console.WriteLine($"Okul: {OkulIsmI}");
+
+
+		}
+
+		public double OgrenciOrtalamaBul()
+		{
+			return (vize1 * 0.3) + (vize2 * 0.3) + (final * 0.4);
+		}
+
+		public string OkulGetir()
+		{
+			return OkulIsmI;
+		}
+
+    }
+}
--- c#EğitimKampı/OgrenciUygulaması/Program.cs
+++ c#EğitimKampı/OgrenciUygulaması/Program.cs
@@ -10,6 +10,72 @@ namespace OgrenciUygulaması
 	{
 		static void Main(string[] args)
 		{
+            // Kullanıcıdan öğrenci bilgilerini al 
+
+            Console.WriteLine("Öğrencinin adı : ");
+			string isim = Console.ReadLine();
+
+            Console.WriteLine("Öğrencinin Soyadı :");
+			string soyisim = Console.ReadLine();
+
+            Console.WriteLine("1. Vize Notu :");
+			double vize1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("2. Vize Notu :");
+			double vize2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Final Notu :");
+			double final = double.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Okul Adı :");
+			string OkulIsmi = Console.ReadLine();
+
+
+			Ogrenci ogrenci = new Ogrenci(isim,soyisim,vize1,vize2,final,OkulIsmi);
+
+
+			while (true)
+			{
+				Console.WriteLine("\n--- MENÜ ---");
+				Console.WriteLine("1. Öğrenci bilgilerini göster");
+				Console.WriteLine("2. Öğrenci ortalamasını göster");
+				Console.WriteLine("3. Öğrencinin okulunu öğren");
+				Console.WriteLine("4. Çıkış yap");
+				Console.Write("Seçiminizi yapın: ");
+				string secim = Console.ReadLine();
+
+				if (secim == "1")
+				{
+					ogrenci.OgrenciBilgileriGoster();
+
+				}
+				else if (secim == "2")
+				{
+					double ortalama = ogrenci.OgrenciOrtalamaBul();
+					Console.WriteLine($"Öğrencin Ortalaması : {ortalama}");
+
+				}
+				else if (secim == "3") {
+
+                    Console.WriteLine($"Öğrencinin Okulu : {OkulIsmi}");
+				}
+				else if (secim == "4")
+				{
+					Console.WriteLine("Programdan çıkılıyor...");
+					break;
+				}
+				else
+				{
+					Console.WriteLine("Geçersiz seçim! Lütfen tekrar deneyin.");
+				}
+
+
+
+
+			}
+
+			Console.ReadLine();
 		}
 	}
 }
